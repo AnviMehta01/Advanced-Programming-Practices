3da1937279c55094c07cc46957ac9f3e27f03486
AnviMehta01 <anvim452@gmail.com>
2026-07-31 13:59:06 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n, original, reverse = 0;
+
+        System.out.print("Enter a number: ");
+        n = sc.nextInt();
+
+        original = n;
+
+        while (n != 0) {
+            int digit = n % 10;
+            reverse = reverse * 10 + digit;
+            n = n / 10;
+        }
+
+        if (original == reverse)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+
+        sc.close();
+    }
+}
---
+++ PrimeNumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        boolean prime = true;
+
+        System.out.print("Enter a number: ");
+        n = sc.nextInt();
+
+        if (n <= 1) {
+            prime = false;
+        } else {
+            for (int i = 2; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+        }
+
+        if (prime)
+            System.out.println("Prime Number");
+        else
+            System.out.println("Not a Prime Number");
+
+        sc.close();
+    }
+}
