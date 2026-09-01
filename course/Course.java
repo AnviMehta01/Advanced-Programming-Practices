be2b7cea46d46cdecd8a92586337b9553a77318b
AnviMehta01 <anvim452@gmail.com>
2026-09-01 14:01:17 +0530

    Create Course.java

---
+++ course/Course.java
@@ -0,0 +1,16 @@
+package week6.course;
+
+public class Course {
+    String courseName;
+    String courseCode;
+
+    public Course(String courseName, String courseCode) {
+        this.courseName = courseName;
+        this.courseCode = courseCode;
+    }
+
+    public void displayCourse() {
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Course Code: " + courseCode);
+    }
+}
